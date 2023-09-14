49785dfba37a43046f75074f3cd7b938de767a3f
Console-buche <dubourg.sebastien@gmail.com>
2023-09-15 00:04:16 +0200

    chore: update project config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,64 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:unicorn/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+  },
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "variableLike",
+        format: ["strictCamelCase", "PascalCase", "UPPER_CASE"],
+        leadingUnderscore: "allow",
+        trailingUnderscore: "allow",
+      },
+      {
+        selector: "variable",
+        types: ["boolean"],
+        format: ["camelCase", "PascalCase"],
+        prefix: ["is", "has", "can", "should", "will", "did"],
+      },
+      {
+        selector: ["function"],
+        format: ["camelCase", "PascalCase"],
+        leadingUnderscore: "allow",
+      },
+      {
+        selector: "typeLike",
+        format: ["PascalCase"],
+      },
+    ],
+    "@typescript-eslint/no-unused-expressions": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { varsIgnorePattern: "_" }],
+    "@typescript-eslint/no-use-before-define": "off",
+    "arrow-body-style": "off",
+    "unicorn/no-array-callback-reference": "off",
+    "unicorn/no-useless-undefined": "off",
+    "unicorn/prevent-abbreviations": "off",
+    "unicorn/prefer-spread": "off",
+    "unicorn/filename-case": [
+      "error",
+      {
+        cases: {
+          kebabCase: true,
+          pascalCase: true,
+        },
+      },
+    ],
+    "arrow-parens": "off",
+    "comma-dangle": ["error", "never"],
   },
-}
+};
